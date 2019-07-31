e90dfbbacfbf8373a77b34c5251da8b70a9b3ef7
Ryan Yost <ryost217@gmail.com>
2019-07-30 21:10:10 -0500

    handle slack in message to all groups

--- src/handlers/admin/sendMessageToAllGroups.js
+++ src/handlers/admin/sendMessageToAllGroups.js
@@ -1,11 +1,20 @@
 const { GroupServices, PlatformServices } = require("../../services");
 const GroupMeServices = PlatformServices.GroupMe;
+const { WebClient } = require("@slack/web-api");
 
 module.exports = async message => {
   const groups = await GroupServices.findAllGroups();
 
   for (let group of groups) {
-    await GroupMeServices.sendBotMessage(message, group.bot.bot_id);
+    if (group.platform === "groupme") {
+      await GroupMeServices.sendBotMessage(message, group.bot.bot_id);
+    } else if (group.platform === "slack") {
+      const client = new WebClient(group.bot.bot_access_token);
+      await client.chat.postMessage({
+        channel: group.slackId,
+        text: message
+      });
+    }
   }
 
   return message;

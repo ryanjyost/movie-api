9d41f3d5dca12f33dfd91c53d8e128253bca2bf6
Ryan Yost <ryost217@gmail.com>
2019-10-17 19:37:13 -0500

    cleanup

--- db.js
+++ db.js
@@ -5,9 +5,6 @@ mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
-// const URL =
-//   "mongodb://skoosh:skoosh2002@ds127362.mlab.com:27362/newsbie-sandbox";
-
 mongoose.connect(
   URL,
   { useNewUrlParser: true }
